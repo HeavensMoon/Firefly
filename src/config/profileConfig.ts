4a158700a1ba2a0d1603d8e28894a05fc3227e6e
HeavensMoon <3145688793@qq.com>
2026-05-08 21:15:56 +0800

    更新个人信息

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -12,7 +12,7 @@ export const profileConfig: ProfileConfig = {
 	name: "Firefly",
 
 	// 个人签名
-	bio: "Hello, I'm Firefly.",
+	bio: "胸中一点浩然气，天地千里快哉风",
 
 	// 链接配置
 	// 已经预装的图标集：fa7-brands，fa7-regular，fa7-solid，material-symbols，simple-icons
@@ -24,19 +24,19 @@ export const profileConfig: ProfileConfig = {
 		{
 			name: "qq",
 			icon: "fa7-brands:qq",
-			url: "https://qm.qq.com/q/ZGsFa8qX2G",
+			url: "https://qm.qq.com/q/w814KAI1pg",
 			showName: false,
 		},
 		{
 			name: "GitHub",
 			icon: "fa7-brands:github",
-			url: "https://github.com/CuteLeaf",
+			url: "https://github.com/HeavensMoon",
 			showName: false,
 		},
 		{
 			name: "Email",
 			icon: "fa7-solid:envelope",
-			url: "mailto:xiaye@msn.com",
+			url: "mailto:3145688793@qq.com",
 			showName: false,
 		},
 		{
