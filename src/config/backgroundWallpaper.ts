bd3c567144c9afd889194c520e87b20ef3f93dea
HeavensMoon <3145688793@qq.com>
2026-05-10 19:21:13 +0800

    改导航栏

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -66,9 +66,9 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 		// 主页横幅文字
 		homeText: {
 			// 是否启用主页横幅文字
-			enable: false,
+			enable: true,
 			// 是否允许用户通过控制面板切换横幅标题显示
-			switchable: true,
+			switchable: false,
 			// 主页横幅主标题
 			title: "Tomのblog",
 			// 主页横幅主标题字体大小
