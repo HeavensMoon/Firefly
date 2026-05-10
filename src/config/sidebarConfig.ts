e6669fe165dcccb5f6631b8606b89a4f868ef224
HeavensMoon <3145688793@qq.com>
2026-05-10 21:12:49 +0800

    改导航栏

--- src/config/sidebarConfig.ts
+++ src/config/sidebarConfig.ts
@@ -124,7 +124,7 @@ export const sidebarLayoutConfig: SidebarLayoutConfig = {
 			// 组件类型：日历组件
 			type: "calendar",
 			// 是否启用该组件
-			enable: true,
+			enable: false,
 			// 组件位置
 			position: "sticky",
 			// 是否在文章详情页显示
