5ecef3e938c35460d0eb6f7676dd9c71593ce0f5
HeavensMoon <3145688793@qq.com>
2026-05-11 10:43:58 +0800

    添加评论区

--- src/config/commentConfig.ts
+++ src/config/commentConfig.ts
@@ -47,13 +47,13 @@ export const commentConfig: CommentConfig = {
 	//giscus评论系统配置
 	giscus: {
 		// 设置 Giscus 评论系统仓库
-		repo: "CuteLeaf/Firefly",
+		repo: "HeavensMoon/Firefly",
 		// 设置 Giscus 评论系统仓库ID
-		repoId: "R_kgD2gfdFGd",
+		repoId: "R_kgDOSXtqIw",
 		// 设置 Giscus 评论系统分类
-		category: "General",
+		category: "Announcements",
 		// 获取 Giscus 评论系统分类ID
-		categoryId: "DIC_kwDOKy9HOc4CegmW",
+		categoryId: "DIC_kwDOSXtqI84C8wLM",
 		// 获取 Giscus 评论系统映射方式
 		mapping: "title",
 		// 获取 Giscus 评论系统严格模式
